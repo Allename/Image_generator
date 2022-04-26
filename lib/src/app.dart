f2d7cd4aa9959e37791e393a3e28de4dc433eca7
Allename <allename.dev@gmail.com>
2022-04-26 23:20:43 +0100

    created new imageModel instance

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' show get;
+import '../src/models/image_model.dart';
+import 'dart:convert';
 
 class App extends StatefulWidget {
   const App({Key? key}) : super(key: key);
@@ -14,6 +16,7 @@ class AppState extends State<App>{
   void fetchImage() async {
     counter ++;
     var response = await get(Uri.parse('https://jsonplaceholder.typicode.com/photos/$counter'));
+    var imageModel = ImageModel.fromJson(json.decode(response.body));
   }
 
   @override
