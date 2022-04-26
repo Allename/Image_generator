d7bcc6e9eee96b773f1fd8046b9006e939c92c18
Allename <allename.dev@gmail.com>
2022-04-26 20:55:12 +0100

    created fetchImage method in app.dart

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -10,17 +10,18 @@ class App extends StatefulWidget {
 class AppState extends State<App>{
   int counter = 0;
 
+  void fetchImage() {
+
+  }
+
   @override
   Widget build(context) {
     return MaterialApp(
       home: Scaffold(
         floatingActionButton: FloatingActionButton(
           child: const Icon(Icons.add),
-          onPressed: () {
-            setState(() {
-              counter ++;
-            });
-          },
+          // Referencing the fetchImage() method
+          onPressed: fetchImage,
         ),
         appBar: AppBar(
           title: const Text('Lets see some images!'),
