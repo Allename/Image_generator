42068a40fc3088492772988d9084e8c43b7e7449
Allename <allename.dev@gmail.com>
2022-04-28 20:27:13 +0100

    created image_list.dart and ImageList class

---
+++ lib/src/widgets/image_list.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import '../models/image_model.dart';
+
+class ImageList extends StatelessWidget {
+  const ImageList({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
